69140975b5f0c51f71f615a0639f42d8d8baa9b7
Dournbrood <tanner1220002@gmail.com>
2019-12-19 13:21:42 -0500

    added the absolute truth

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>DOURNBROOD IS SIMPLY THE BEST WEB DEVELOPER THERE IS.</h1>
       <h2>
         Deployed with{' '}
         <a
