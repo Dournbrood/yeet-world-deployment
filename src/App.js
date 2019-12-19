ec78d0b14422ea01740d225c44c484b157862c7a
Dournbrood <tanner1220002@gmail.com>
2019-12-19 13:48:37 -0500

    i honestly just don't know any more

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>DOURNBROOD IS SIMPLY THE BEST WEB DEVELOPER THERE IS.</h1>
+      <h1>Yeet world deployment team (minus AJ) says: "Mando is daddy...?"</h1>
       <h2>
         Deployed with{' '}
         <a
